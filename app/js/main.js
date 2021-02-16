9c2f6c93cd39f49683a53950f4cf22ae682151cd
eugeneskom <eugeneskom@gmail.com>
2021-02-16 17:44:49 +0200

    fixed jq of the plan section, now selection works

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function () {
     $('.plan__btn').removeClass('plan__btn--active');
     $(this).addClass('plan__btn--active');
     $('.plan__btn').parents('.plan__card').removeClass('plan__card--active');
-    $('.plan__btn').parents('.plan__card').addClass('plan__card--active');
+    $(this).parents('.plan__card').addClass('plan__card--active');
 
   });
   
