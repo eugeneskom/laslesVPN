9cdfaf9b4f6e31c11c3b50430e4b3ace1fff31fc
eugeneskom <eugeneskom@gmail.com>
2021-02-18 15:40:58 +0200

    added swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function () {
-  
-  $('.plan__btn').on('click', function(e){
+  import Swiper from '../../node_modules/swiper/swiper';
+  // import Swiper styles
+  const swiper = new Swiper('.clients__list', {
+    // Optional parameters
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+  });
+
+
+  $('.plan__btn').on('click', function (e) {
     e.preventDefault();
     $('.plan__btn').removeClass('plan__btn--active');
     $(this).addClass('plan__btn--active');
@@ -8,5 +17,9 @@ $(function () {
     $(this).parents('.plan__card').addClass('plan__card--active');
 
   });
-  
+
+
+
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,8 @@ function scripts() {
     'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+    'node_modules/swiper/swiper-bundle.min.js',
+
     'app/js/main.js'
 
   ])
